1826a8d6d5de1ff76e67a092b4bd6aa1b23da079
SavvaMey <savva28031997@gmail.com>
2020-08-17 00:48:47 +0300

    5. Аннотация @Override [#340472]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
+    //аннотация!
     @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + "  "+ "\"name\" " + ": " + name + System.lineSeparator()
