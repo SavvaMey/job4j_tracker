c5e42a489e095c3faee4f7ef5d8c56cda0bab787
SavvaMey <savva28031997@gmail.com>
2020-08-18 17:50:18 +0300

    6. Метод замены заявки. Tracker.replace [#340482]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,15 +18,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -39,6 +32,27 @@ public class Tracker {
             }
         }
         rsl = Arrays.copyOf(rsl,size);
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index  == -1){
+            return false;
+        }
+        item.setId(items[index].getId());
+        items[index] = item;
+        return true;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
         return rsl;
     }
 }
