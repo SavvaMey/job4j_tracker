bcee4e626a6381aa81f915e1b92ff9748f2788e7
SavvaMey <savva28031997@gmail.com>
2020-08-19 21:15:02 +0300

    4.2. Статические методы. [#340412]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,16 +3,42 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 
 public class StartUI {
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("===  Edit item ====");
+        int id =input.askInt("Enter the id of the changed application:");
+        String name = input.askStr("Enter the new name of the changed application:");
+        Item item = new Item(name);
+        if (tracker.replace(id,item)) {
+            System.out.println("имя заявки изменено");
+        } else {
+            System.out.println("введен неккоректный id");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        int id = input.askInt("Enter the id of the removable application:");
+        if (tracker.delete(id)) {
+            System.out.println("имя заявки изменено");
+        } else {
+            System.out.println("введен неккоректный id");
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int select = input.askInt("Select:");
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
                 System.out.println("=== Output all items ====");
                 Item[] items = tracker.findAll();
@@ -20,22 +46,9 @@ public class StartUI {
                     System.out.println("id: "+ item.getId() + " name: " + item.getName());
                 }
             } else if (select == 2) {
-                System.out.println("===  Edit item ====");
-                int id =input.askInt("Enter the id of the changed application:");
-                String name = input.askStr("Enter the new name of the changed application:");
-                Item item = new Item(name);
-                if (tracker.replace(id,item)) {
-                    System.out.println("имя заявки изменено");
-                } else {
-                    System.out.println("введен неккоректный id");
-                }
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
-                int id = input.askInt("Enter the id of the removable application:");
-                if (tracker.delete(id)) {
-                    System.out.println("имя заявки изменено");
-                } else {
-                    System.out.println("введен неккоректный id");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
                 int id = input.askInt("Enter the id of the application:");
                 Item item = tracker.findById(id);
