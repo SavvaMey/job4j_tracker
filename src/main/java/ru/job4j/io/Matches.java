05f2d001dc54770f2b3d2be8a7b0293079702f55
SavvaMey <savva28031997@gmail.com>
2020-08-19 12:10:24 +0300

    2. Scanner и чтение числа из консоли. [#340406]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -6,34 +6,29 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean run = true;
-        int kon = 1;
+        int igrok = 1;
         int matches = 11;
         int select;
         System.out.println("Спички");
         while (run) {
-            System.out.println("Кон " + kon);
             System.out.println("Осталось спичек " + matches);
-            System.out.print("Игрок 1 возьмите от 1 до 3 спичек: ");
+            System.out.print("Игрок " + igrok  + " возьмите от 1 до 3 спичек: ");
             select = Integer.valueOf(input.nextLine());
+            if (select == 1 || select == 2 || select == 3) {
                 matches -= select;
                 if (matches == 0) {
-                    System.out.println("1 игрок выйграл, спичек осталось: " + matches);
+                    System.out.println(igrok +" игрок выйграл, спичек осталось: " + matches);
                     run = false;
                     break;
                 } else {
                     System.out.println("Спичек осталось " + matches);
                 }
-            System.out.print("Игрок 2 возьмите от 1 до 3 спичек: ");
-            select = Integer.valueOf(input.nextLine());
-            matches -= select;
-            if (matches == 0) {
-                System.out.println("2 игрок выйграл, спичек осталось: " + matches);
-                run = false;
-                break;
+                igrok = (igrok == 1) ? 2 : 1;
             } else {
-                System.out.println("Спичек осталось " + matches);
+                System.out.println("Нельзя брать столько спичек");
+                continue;
             }
-            kon++;
+
         }
     }
 }
