8340d7256ef008e4ddef73d6638e31b99ff5298a
SavvaMey <savva28031997@gmail.com>
2020-08-22 11:02:32 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#340422]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -175,25 +175,13 @@ public class StartUITest {
     }
 
     @Test
-    public void whenInvalidExit() {
+    public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] { "1" , "0" }
+                new String[] {"one","1"}
         );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new ExitAction(out)
-        };
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                String.format(
-                        "Menu.%n"
-                                + "0. Exit%n"
-                                + "Wrong input, you can select: 0 .. 0%n"
-                                + "Menu.%n"
-                                + "0. Exit%n"
-                                + "=== Exit ====%n"
-                )
-        ));
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
     }
 }
