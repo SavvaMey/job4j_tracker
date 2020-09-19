6c3f6f9472c34894cb9407e5c165aaf36d32db8b
SavvaMey <savva28031997@gmail.com>
2020-09-19 11:28:58 +0300

    4. Преобразование List в Map. [#340225],5. Преобразование матрицы чисел в список чисел. [#340226]

--- src/main/java/ru/job4j/StreamApi/ListToMap.java
+++ src/main/java/ru/job4j/StreamApi/ListToMap.java
@@ -1,22 +1,17 @@
 package ru.job4j.StreamApi;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListToMap {
-    public static void main(String[] args) {
-        List<Student> studentList = List.of(
-                new Student("key", 50),
-                new Student("dan",40),
-                new Student("bim", 48),
-                new Student("key", 40)
-        );
-        Map<String, Integer> map = studentList.stream()
+    public static Map<String, Integer> listToMap(List<Student> list) {
+        return list.stream()
                 .collect(Collectors.toMap(Student::getSurname, Student::getScore,
-                        (address1, address2) -> address1 ));
-        map.forEach((k,v) -> System.out.println(k + " " + v));
+                        (address1, address2) -> address1));
+    }
+
+    public static List<Integer> listInt(Integer[][] matrix) {
+        return Stream.of(matrix).flatMap(Stream::of).collect(Collectors.toList());
     }
 }
---
+++ src/test/java/ru/job4j/StreamApi/ListToMapTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.StreamApi;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ListToMapTest {
+
+    @Test
+    public void listToMap() {
+        List<Student> studentList = List.of(
+                new Student("key", 50),
+                new Student("dan", 40),
+                new Student("bim", 48),
+                new Student("key", 40)
+        );
+        Map<String, Integer> map = ListToMap.listToMap(studentList);
+        Map<String, Integer> mapExpected = Map.of(
+                "key", 50,
+                "dan", 40,
+                "bim", 48
+        );
+        assertThat(mapExpected, is(map));
+    }
+
+    @Test
+    public void MatrixToList() {
+        Integer[][] arr = {{1, 2, 3}, {1, 2}};
+        List<Integer> list = ListToMap.listInt(arr);
+        List<Integer> listExpected = List.of(1, 2, 3, 1, 2);
+        assertThat(listExpected, is(list));
+    }
+}
