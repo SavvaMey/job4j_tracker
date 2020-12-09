ef47e862c39a1940def1f27ceb603c5212635ebc
SavvaMey <savva28031997@gmail.com>
2020-12-10 01:32:48 +0300

    2. Трекер SQL [#372748]

---
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -0,0 +1,73 @@
+package ru.job4j.tracker;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store {
+    private Connection cn;
+
+    public void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        try(PreparedStatement statement = cn.prepareStatement("INSERT INTO items (name) VALUES (?)", new String[] {"id"})) {
+            statement.setString(1, item.getName());
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                item.setId(rs.getInt("id"));
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return item;
+    }
+
+    @Override
+    public boolean replace(String id, Item item) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        return null;
+    }
+
+    @Override
+    public Item findById(String id) {
+        return null;
+    }
+}
